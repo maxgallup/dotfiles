cab7ad094685c8416c86d6041270eaf096967629
maxgallup <m.gallup@student.vu.nl>
2022-08-21 18:26:46 +0200

    updated scripts

---
+++ scripts/updatedefaultapp.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+filetypes=(
+"text/plain" 
+"text/css"
+"text/javascript"
+"text/mathml"
+"text/x-c++hdr"
+"text/x-c++src"
+"text/x-csrc"
+"text/x-chdr"
+"text/x-dtd"
+"text/x-java"
+"text/x-javascript"
+"text/x-makefile"
+"text/x-moc"
+"text/x-pascal"
+"text/x-patch"
+"text/x-perl"
+"text/x-php"
+"text/x-python"
+"text/x-sql"
+"text/x-tcl"
+"text/x-tex"
+"text/xml"
+"application/javascript"
+"application/x-cgi"
+"application/x-javascript"
+"application/x-perl"
+"application/x-php"
+"application/x-python"
+"application/x-shellscript"
+"application/xml"
+"application/xml-dtd"
+)
+
+
+for type in "${filetypes[@]}"; do
+  xdg-mime default lvim.desktop $type
+  xdg-mime query default $type
+done
+
