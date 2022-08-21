fd075c018cc569b28c74ee4094c60689c12450d8
maxgallup <m.gallup@student.vu.nl>
2022-08-21 17:39:58 +0200

    n

--- config/.config/lvim/config.lua
+++ config/.config/lvim/config.lua
@@ -34,10 +34,10 @@ lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 -- vim.keymap.set("v", "i", "k")
 -- vim.keymap.set("v", "k", "j")
 
--- vim.keymap.set("n", "<C-j>", "<C-w>h")
--- vim.keymap.set("n", "<C-k>", "<C-w>j")
--- vim.keymap.set("n", "<C-i>", "<C-w>k")
--- vim.keymap.set("n", "<C-l>", "<C-w>l")
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
 
 -- vim.keymap.set("n", "<C-n>", ":vsplit<cr>")
 
@@ -99,9 +99,9 @@ function _G.set_terminal_keymaps()
   local opts = { buffer = 0 }
   -- vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
   vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
-  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd h<CR>]], opts)
-  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd j<CR>]], opts)
-  vim.keymap.set('t', '<C-i>', [[<Cmd>wincmd k<CR>]], opts)
+  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
   vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
 end
 
