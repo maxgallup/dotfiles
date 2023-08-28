d8ed4469cf435fc0fdec0e4af879b41d8bb67fdc
maxgallup <m.gallup@student.vu.nl>
2023-08-28 23:26:10 +0200

    changed fonts

--- scripts/stow-update.sh
+++ scripts/stow-update.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+rm /home/max/.bash_logout
+rm /home/max/.bashrc
+rm /home/max/.bash_aliases
+rm /home/max/.profile
+rm /home/max/.ssh/config
+rm /home/max/.config/gtk-3.0/bookmarks
 stow config/ --target=/home/max
+
+shopt -s dotglob
+rm /home/max/bin/*
 stow bin/ --target=/home/max/bin
+
 
