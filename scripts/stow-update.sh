64a8ac8e0daba3306192f760da2f41766434ce31
maxgallup <m.gallup@student.vu.nl>
2023-08-28 23:59:47 +0200

    added role

--- scripts/stow-update.sh
+++ scripts/stow-update.sh
@@ -6,6 +6,8 @@ rm /home/max/.bash_aliases
 rm /home/max/.profile
 rm /home/max/.ssh/config
 rm /home/max/.config/gtk-3.0/bookmarks
+rm /home/max/.gitconfig
+
 stow config/ --target=/home/max
 
 shopt -s dotglob
