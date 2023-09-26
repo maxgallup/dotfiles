67388032bf818248bba4babffc89ef3ebc0aea6f
maxgallup <maxgallup@pm.me>
2023-09-26 15:31:18 +0200

    update

--- scripts/setgnometheme.sh
+++ scripts/setgnometheme.sh
@@ -2,10 +2,16 @@
 
 if [ $1 -e "green" ]; then
 	echo "green"
+	# GREEN
+	# picture-uri-dark='file:///home/max/dotfiles/pics/monterey-dark-green.jpg'
+	# hint-color-rgba='rgba(120,245,182,0.925676)'
 fi
 
 if [ $1 -e "orange" ]; then
 	echo "orange"
+	# ORANGE
+	# picture-uri-dark='file:///home/max/dotfiles/pics/monterey-orange.jpg'
+	# hint-color-rgba='rgba(120,245,182,0.925676)'
 fi
 
 if [ $1 -e "purple" ]; then
@@ -13,11 +19,4 @@ if [ $1 -e "purple" ]; then
 fi
 
 
-# GREEN
-# picture-uri-dark='file:///home/max/dotfiles/pics/monterey-dark-green.jpg'
-# hint-color-rgba='rgba(120,245,182,0.925676)'
 
-
-# ORANGE
-# picture-uri-dark='file:///home/max/dotfiles/pics/monterey-orange.jpg'
-# hint-color-rgba='rgba(120,245,182,0.925676)'
