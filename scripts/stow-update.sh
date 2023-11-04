4671c48e4fc529f4075cabab0b1924d6567ce66c
maxgallup <maxgallup@pm.me>
2023-11-04 14:26:31 +0100

    code and codium now work properly

--- scripts/stow-update.sh
+++ scripts/stow-update.sh
@@ -2,7 +2,9 @@
 
 mkdir /home/max/.ssh
 mkdir /home/max/.config/Code
+mkdir /home/max/.config/Code/User
 mkdir /home/max/.config/VSCodium
+mkdir /home/max/.config/VSCodium/User
 mkdir /home/max/bin
 
 rm /home/max/.bash_logout
